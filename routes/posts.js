2f3bb5516c8d01b8b61061901fa9133883d68446
Andres <andres039039@hotmail.com>
2022-02-23 20:14:05 -0500

    add error handling for new URL parameters/queries

--- routes/posts.js
+++ routes/posts.js
@@ -4,13 +4,19 @@ import { returnBlogs } from "../models/index.js";
 const router = Router();
 
 router.get("/", async (req, res) => {
+  if (req.query.sortBy || req.query.direction) {
+    return res.status(400).json({
+      error: "sortBy parameter is invalid",
+    });
+  }
   if (!req.query.tag) {
     return res.status(400).json({ error: "Tags parameter is required" });
   }
+
   const tags = req.query.tag.split(",");
   const content = await returnBlogs(tags);
   console.log(tags);
   res.status(200).json({ posts: content });
 });
 
-export default router
+export default router;
